2c4d2ef0da9c8a401cb18526bd7f70ab80b30fca
Aman <2468amansingh@gmail.com>
2025-07-23 23:53:27 +0530

    Did some changes

---
+++ app.js
@@ -0,0 +1,12 @@
+// Show an alert when the page loads
+window.onload = function() {
+    alert("Welcome to My Website!");
+};
+
+// Log a message when any navigation link is clicked
+document.querySelectorAll('nav a').forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        console.log('Navigation link clicked:', event.target.textContent);
+    });
+});
+dhd
